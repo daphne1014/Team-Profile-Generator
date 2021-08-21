aa73e07e45fce3d913db98c4870c754151991782
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-08-21 15:45:40 -0700

    modified html

--- src/page-template.js
+++ src/page-template.js
@@ -95,7 +95,9 @@ function generateHtml(team) {
 
         <body>
         <div class="px-4 py-5 my-5 text-center">
+        <header class="px-4 py-5 my-5 text-center danger">
         <h1 class="display-5 fw-bold">My Team</h1>
+        </header>
          </div>
             <div class="container">
                 ${generateTeam(team)}
